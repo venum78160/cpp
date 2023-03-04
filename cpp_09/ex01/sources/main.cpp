a858cb210b8e172a311f4bed300c6440a66d1023
Valentin L'hotellier <vl-hotel@student.42nice.fr>
2023-03-04 20:41:04 +0100

    cpp_09 ex01

--- cpp_09/ex01/sources/main.cpp
+++ cpp_09/ex01/sources/main.cpp
@@ -1,128 +1,86 @@
-#include "../includes/BitcoinExchange.hpp"
+#include "../includes/RPN.hpp"
 
-bool	isValidDate(const std::string& dateStr)
+void	case_operator(std::stack<int> *my_stack, char *token)
 {
-    if (dateStr.size() != 10)
-        return false;
-
-    if (dateStr[4] != '-' || dateStr[7] != '-')
-        return false;
-
-    int year = atoi(dateStr.substr(0, 4).c_str());
-    int month = atoi(dateStr.substr(5, 2).c_str());
-    int day = atoi(dateStr.substr(8, 2).c_str());
-
-    if (year < 1900 || year > 2100)
-        return false;
-
-    if (month < 1 || month > 12)
-        return false;
-
-    if (day < 1 || day > 31)
-        return false;
-
-    if (month == 4 || month == 6 || month == 9 || month == 11)
-    {
-        if (day > 30)
-            return false;
-    }
-    else if (month == 2)
-    {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-        {
-            if (day > 29)
-                return false;
+	if ((*my_stack).size() < 2)
+	{
+		std::cerr << "Error: missing operands in the stack." << std::endl;
+		exit (1);
+	}
+	int second = (*my_stack).top(); (*my_stack).pop();
+	int first = (*my_stack).top(); (*my_stack).pop();
+	switch (token[0])
+	{
+	case '+':
+		(*my_stack).push(first + second);
+		break;
+	case '-':
+		(*my_stack).push(first - second);
+		break;
+	case '*':
+		(*my_stack).push(first * second);
+		break;
+	case '/':
+		if (second == 0)
+		{
+			std::cerr << "Error: division by zero." << std::endl;
+			exit (1);
         }
-        else
-        {
-            if (day > 28)
-                return false;
-        }
-    }
-    return true;
+		(*my_stack).push(first / second);
+		break;
+	default:
+		std::cerr << "Error: Problem with operator" << std::endl;
+		exit (1);
+		break;
+	}
 }
 
-int main( int argc, char **argv)
+int main(int argc, char **argv)
 {
-	std::map<std::string, double> maps;
-	std::string line;
 	if (argc != 2)
 	{
-		std::cout << "Error: incorrect number of arguments. Please insert a file" << std::endl;
+		std::cout << "Error: incorrect number of arguments." << std::endl;
 		return (1);
 	}
-	std::ifstream fexrate("exchange_rate.txt");
-	if(!fexrate.is_open())
+	std::stack<int> my_stack;
+
+	char *token = std::strtok(argv[1], " ");
+	while (token != NULL)
 	{
-		std::cout << "Please insert a correct file" << std::endl;
-		return (1);
-	}
-	while (getline(fexrate, line))
-	{
-		std::string dateStr = line.substr(0, line.find("|") - 1);
-        std::string valueStr = line.substr(line.find("|") + 2);
-		
-		try
+		if(isdigit(token[0]))
 		{
-			double value = std::stod(valueStr);
-			std::cout << dateStr << " => " << value << " = " << valueStr << std::endl;
-			if (value < 0 || value > 1000)
-				throw std::out_of_range("not in valid range");
-			if (!isValidDate(dateStr))
-				throw std::invalid_argument("❌ Date is not valid.");
-			maps[dateStr] = value;
+			int num = atoi(token);
+			std::cerr << "Debug[]: num = "<< num << " my_stack.size = "<< my_stack.size()<< std::endl;
+			if (num >= 0 && num <= 9)
+			{
+				std::cerr << "Debug[]: here num = " << num << std::endl;
+				my_stack.push(num);
+			}
+			else if (num < 100 && num >= 0 && my_stack.size() == 0)
+			{
+				my_stack.push(num / 10);
+				my_stack.push(num % 10);
+			}
+			else
+			{
+				std::cerr << "Error: numbers must be less than 10. num ="<< num << std::endl;
+				return (1);
+			}
 		}
-		catch(const std::exception& e)
+		else if (strlen(token) == 1 && (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/'))
+			case_operator(&my_stack, token);
+		else
 		{
-			std::cerr << e.what() << " => " << line << std::endl;
+			std::cerr << "Error: invalid token => "<< token << std::endl;
+				return (1);
 		}
+		token = strtok(NULL, " ");
 	}
-	std::cout << "----------------------------------" << std::endl;
-	line.clear();
-	std::ifstream file(argv[1]);
-	std::cout << std::fixed;
-    std::cout.precision(2);
-	if(!file.is_open())
-	{
-		std::cout << "Please insert a correct file" << std::endl;
-		return (1);
-	}
-	while (getline(file, line))
-	{
-		std::string dateStr = line.substr(0, line.find("|") - 1);
-        std::string valueStr = line.substr(line.find("|") + 2);
-		
-		try
+	if (my_stack.size() != 1)
 		{
-			double value = std::stod(valueStr);
-			if (value < 0 || value > 1000)
-				throw std::out_of_range("not in valid range");
-			if (!isValidDate(dateStr))
-				throw std::invalid_argument("❌ Date is not valid.");
-			maps[dateStr] = value;
-			std::map<std::string, double>::iterator it = maps.find(dateStr);
-			if (it == maps.end())
-				throw std::invalid_argument("❌ Date has not been found.");
-			double rate = maps[dateStr];
-            double convertedValue = value * rate;
-            std::cout << dateStr << " => " << value << " * rate(" << rate <<") = " << convertedValue << std::endl;
+			std::cerr << "Missing an operator"<< token << std::endl;
+				return (1);
 		}
-		catch(const std::exception& e)
-		{
-			std::cerr << "Error : " <<e.what() << " => " << line << std::endl;
-		}
-	}
-	return 0;
-	// try
-	// {
-	// 	double value = std::stod(valueStr);
-	// 	if (value < 0 || value > 1000)
-	// 		throw std::out_of_range("not in valid range");
-	// 	if (!isValidDate(dateStr))
-	// 		throw std::invalid_argument("❌ Date is not valid.");
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
+	std::cout << my_stack.top() << std::endl;
+	return (0);
 }
